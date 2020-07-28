7d82a16b4e1e8b1073f71aa97ea1bd8f4dc9e9c4
Steph Phoa <steph.phoa@gmail.com>
2020-07-29 03:00:58 +0800

    please commit pls push

---
+++ code.R
@@ -0,0 +1,7 @@
+#Git actions:
+#pull: pull changes from remote repo (if you are in collaboration with others)
+#add: stage files for commit; click on stage box under git pane
+#commit: commit to the local repo; click on "commit" button under git pane; add a commit message
+#push: push to the remote repo on Github
+
+library(tidyverse)
